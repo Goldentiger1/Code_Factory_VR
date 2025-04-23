5975509c8c06fbe128bc43c1296c3e869e23949b
Aaro Leskinen <aaro.leskinen@kolumbus.fi>
2025-04-23 16:38:49 +0300

    Did not manage to hands work quite yet in vr

--- build/Assets/ButtonSpawn.cs
+++ build/Assets/ButtonSpawn.cs
@@ -2,66 +2,83 @@ using Godot;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
 
 public partial class ButtonSpawn : Node3D
 {
-    // A public Node3D that contains our boxes
-    public Node3D nodeBoxes = new Node3D();
-    // A public list for boxes from Node3 boxes container
-    public Node3D[] listBoxes = new Node3D[5];
-    // A private PackedScene of our box
-    private PackedScene boxScene = new PackedScene();
-    // A public Node3D of our box
-    public Node3D box = new Node3D();
-    // A private vector3 for our box spawn position
-    public Vector3 boxPos = new Vector3(3.0f, 1.45f, -4.0f);
-    // A private boolean value to see is button pressed
-    private bool SButtonPressed = false;
-    // A private variable for list length
-    private int lBoxeslength = 0;
+	// A public Node3D that contains our boxes
+	public Node3D nodeBoxes = new Node3D();
+	// A public list for boxes from Node3 boxes container
+	public Node3D[] listBoxes = new Node3D[5];
+	// A private PackedScene of our box
+	private PackedScene boxScene = new PackedScene();
+	// A public Node3D of our box
+	public Node3D box = new Node3D();
+	// A private Area3D for our buttonTrigger
+	private Area3D sBoxTrigger = new Area3D(); 
+	// A private vector3 for our box spawn position
+	public Vector3 boxPos = new Vector3(3.0f, 1.45f, -4.0f);
+	// A private boolean value to see is button pressed
+	private bool sButtonPressed = false;
+	// A private variable for list length
+	private int lBoxeslength = 0;
 
-    public override void _Ready()
-    {
-        // Set a name for our box container
-        nodeBoxes.Name = "Boxes";
-        // Set our box containers position in the scene.
-        nodeBoxes.Position = new Vector3(0.0f, 0.0f, 0.0f);
-        // Wait for main Node3D to be free with CallDeferred()
-        // and then add our Node3D boxes to the scene
-        GetNode<Node3D>("../..").CallDeferred("add_child", nodeBoxes);
-        // Preload our box PackedScene
-        boxScene = GD.Load<PackedScene>("res://Assets/Box.tscn");
-        // Set the value into lBoxeslength of our listBoxes
-        lBoxeslength = listBoxes.Length;
+	public override void _Ready()
+	{
+		// Set a name for our box container
+		nodeBoxes.Name = "Boxes";
+		// Set our box containers position in the scene.
+		nodeBoxes.Position = new Vector3(0.0f, 0.0f, 0.0f);
+		// Wait for main Node3D to be free with CallDeferred()
+		// and then add our Node3D boxes to the scene
+		GetNode<Node3D>("../..").CallDeferred("add_child", nodeBoxes);
+		// Preload our box PackedScene
+		boxScene = GD.Load<PackedScene>("res://Assets/Box.tscn");
+		// Set the value into lBoxeslength of our listBoxes
+		lBoxeslength = listBoxes.Length;
+		// Set our StartButtons value into sBoxTrigger
+		sBoxTrigger = GetNode<Area3D>("ButtonTrigger3D");
+		// Get Area entered
+		sBoxTrigger.BodyEntered += OnButtonTriggerEntered;
+	}
+
+	public override void _Process(double delta)
+	{
+
+		switch(sButtonPressed)
+		{
+			case false when Input.IsActionJustPressed("Key_A"):
+				sButtonPressed = true;
+				GD.Print(sButtonPressed);                
+				break;
+			case true when Input.IsActionJustPressed("Key_A"):
+				sButtonPressed = false;
+				GD.Print(sButtonPressed);
+				break;
+		}
+		if(sButtonPressed == true)
+		{
+			if (listBoxes[0] == null)
+			{
+				for(int i = 0; i <= lBoxeslength; i++)
+				{
+					listBoxes[i] = new Node3D();
+					box = boxScene.Instantiate<Node3D>();
+					box.Name = "Box_" + (i + 1);
+					box.Position = boxPos;
+					listBoxes[i] = box;
+				}
+			}
+		}
+
+	}
+
+	private void OnButtonTriggerEntered(Node3D hand)
+	{
+		GD.Print(hand);
+		GD.Print("Entered");
     }
 
-    public override void _Process(double delta)
-    {
-        switch(SButtonPressed)
-        {
-            case false when Input.IsActionJustPressed("Key_A"):
-                SButtonPressed = true;
-                GD.Print(SButtonPressed);                
-                break;
-            case true when Input.IsActionJustPressed("Key_A"):
-                SButtonPressed = false;
-                GD.Print(SButtonPressed);
-                break;
-        }
-        if(SButtonPressed == true)
-        {
-            if (listBoxes[0] == null)
-            {
-                for(int i = 0; i <= lBoxeslength; i++)
-                {
-                    listBoxes[i] = new Node3D();
-                    box = boxScene.Instantiate<Node3D>();
-                    box.Name = "Box_" + (i + 1);
-                    box.Position = boxPos;
-                    listBoxes[i] = box;
-                }
-            }
-        }
 
-    }
 }
+
--- build/Assets/RunButton.cs
+++ build/Assets/RunButton.cs
@@ -6,92 +6,92 @@ using System.Linq;
 
 public partial class RunButton : Node3D
 {
-    // A private Node3D variable for our StartButton
-    private ButtonSpawn startButton = new ButtonSpawn();
-    // A private boolean value to see is button pressed
-    private bool RButtonPressed = false;
-    // A private Vector3 value to see is box spawn position free
-    private Vector3 SBoxPos = new Vector3(0.0f, 0.0f, 0.0f);
-    // A private compare value
-    private Vector3 ComBoxPos = new Vector3(0.0f, 0.0f, 0.0f);
-    // A private Vector3 list for box positions to go through
-    private Vector3[] LBoxPos = new Vector3[5];
-    // A private float time to make code wait a little before
-    // going forward
-    float time = 0.1f;
+	// A private Node3D variable for our StartButton
+	private ButtonSpawn startButton = new ButtonSpawn();
+	// A private boolean value to see is button pressed
+	private bool RButtonPressed = false;
+	// A private Vector3 value to see is box spawn position free
+	private Vector3 SBoxPos = new Vector3(0.0f, 0.0f, 0.0f);
+	// A private compare value
+	private Vector3 ComBoxPos = new Vector3(0.0f, 0.0f, 0.0f);
+	// A private Vector3 list for box positions to go through
+	private Vector3[] LBoxPos = new Vector3[5];
+	// A private float time to make code wait a little before
+	// going forward
+	float time = 0.1f;
 
-    public override void _Ready()
-    {
-        // Get our boxes from StartButton
-        startButton = GetNode<ButtonSpawn>("../StartButton");
-    }
+	public override void _Ready()
+	{
+		// Get our boxes from StartButton
+		startButton = GetNode<ButtonSpawn>("../StartButton");
+	}
 
-    public override void _Process(double delta)
-    {
-        // See if button B is pressed
-        switch(RButtonPressed)
-        {
-            case false when Input.IsActionJustPressed("Key_B"):
-                RButtonPressed = true;
-                GD.Print(RButtonPressed);
-                break;
-            case true when Input.IsActionJustPressed("Key_B"):
-                RButtonPressed = false;
-                GD.Print(RButtonPressed);
-                break;
-        }
+	public override void _Process(double delta)
+	{
+		// See if button B is pressed
+		switch(RButtonPressed)
+		{
+			case false when Input.IsActionJustPressed("Key_B"):
+				RButtonPressed = true;
+				GD.Print(RButtonPressed);
+				break;
+			case true when Input.IsActionJustPressed("Key_B"):
+				RButtonPressed = false;
+				GD.Print(RButtonPressed);
+				break;
+		}
 
-        if(RButtonPressed == true)
-        {
-            if (startButton.listBoxes[0] == null)
-            {
-                GD.Print("Start level first");
-                RButtonPressed = false;
-            }
-            else
-            {
-                for(int i = 0; i < startButton.listBoxes.Length; i++)
-                {
-                    if (SBoxPos.X <= (startButton.boxPos.X - 3.0f))
-                    {
-                        startButton.nodeBoxes.AddChild(startButton.listBoxes[i]);
-                        LBoxPos[i] = startButton.listBoxes[i].Position;
-                        SBoxPos = startButton.listBoxes[i].Position;
-                        GD.Print("Loop number: " + i + "\n" + "Current box name: " + startButton.listBoxes[i].Name);
-                        continue;
-                    }
-                    else if(SBoxPos.X <= startButton.boxPos.X)
-                    {
-                        for(int j = 0; j < LBoxPos.Length; j++)
-                        {
-                            if (LBoxPos[j] == ComBoxPos)
-                            {
-                                continue;
-                            }
-                            else
-                            {
-                                LBoxPos[j] -=  new Vector3(0.1f, 0.0f, 0.0f);
-                                startButton.listBoxes[j].Position = LBoxPos[j];
-                                SBoxPos = startButton.listBoxes[i].Position;
-                                GD.Print("Loop number: " + i + "\n" + "List positions number: " + j + "\n" + "Position: " + LBoxPos[j]);
-                                GD.Print(time);
-                                if (time < 50000.0f)
-                                {
-                                    time += time * (float)delta;
-                                }
-                                else if(time == 50000.0f)
-                                {
-                                    GD.Print(time);
-                                    time = 0.1f;
-                                    continue;
-                                }
-                                    
-                            }
-                        }
-                    }
+		if(RButtonPressed == true)
+		{
+			if (startButton.listBoxes[0] == null)
+			{
+				GD.Print("Start level first");
+				RButtonPressed = false;
+			}
+			else
+			{
+				for(int i = 0; i < startButton.listBoxes.Length; i++)
+				{
+					if (SBoxPos.X <= (startButton.boxPos.X - 3.0f))
+					{
+						startButton.nodeBoxes.AddChild(startButton.listBoxes[i]);
+						LBoxPos[i] = startButton.listBoxes[i].Position;
+						SBoxPos = startButton.listBoxes[i].Position;
+						GD.Print("Loop number: " + i + "\n" + "Current box name: " + startButton.listBoxes[i].Name);
+						continue;
+					}
+					else if(SBoxPos.X <= startButton.boxPos.X)
+					{
+						for(int j = 0; j < LBoxPos.Length; j++)
+						{
+							if (LBoxPos[j] == ComBoxPos)
+							{
+								continue;
+							}
+							else
+							{
+								LBoxPos[j] -=  new Vector3(0.1f, 0.0f, 0.0f);
+								startButton.listBoxes[j].Position = LBoxPos[j];
+								SBoxPos = startButton.listBoxes[i].Position;
+								GD.Print("Loop number: " + i + "\n" + "List positions number: " + j + "\n" + "Position: " + LBoxPos[j]);
+								GD.Print(time);
+								if (time < 50000.0f)
+								{
+									time += time * (float)delta;
+								}
+								else if(time == 50000.0f)
+								{
+									GD.Print(time);
+									time = 0.1f;
+									continue;
+								}
+									
+							}
+						}
+					}
 
-                }
-            }
-        }
-    }
+				}
+			}
+		}
+	}
 }
