e4b5b32adfd538afc687c8b0b469312be8cc4472
Aaro Leskinen <aaro.leskinen@kolumbus.fi>
2025-05-05 09:56:44 +0300

    Cleaned and fixed the codes. Still need to fix some code

--- build/Assets/ButtonSpawn.cs
+++ build/Assets/ButtonSpawn.cs
@@ -1,70 +1,114 @@
 using Godot;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel.Design;
-using System.Runtime.CompilerServices;
 
 public partial class ButtonSpawn : Node3D
 {
-	// A public Node3D that contains our boxes
-	public Node3D nodeBoxes = new Node3D();
-	// A public list for boxes from Node3 boxes container
-	public Node3D[] listBoxes = new Node3D[5];
-	// A private PackedScene of our box
-	private PackedScene boxScene = new PackedScene();
-	// A public Node3D of our box
+	// A public Node3D in our scene to store the boxes cleanly
+	public Node3D node3DBoxes = new Node3D();
+	// A public Node3D list of our boxes which we iterate through
+	// to get our boxes and their values
+	public Node3D[] lNode3DBoxes = new Node3D[5];
+	// A public single Node3D of our box
 	public Node3D box = new Node3D();
-	// A private vector3 for our box spawn position
-	public Vector3 boxPos = new Vector3(3.0f, 1.45f, -4.0f);
-	// A private boolean value to see is button pressed
-	private bool sButtonPressed = false;
-	// A private variable for list length
-	private int lBoxeslength = 0;
-
+	// A public Vector3 position of our pre defined boxes spawn position
+	public Vector3 pdSpawnPos = new Vector3(3.0f, 1.45f, -4.0f);
+    // A public boolean variable to see is button pressed
+    public bool sButtonPressed = false;
+	// A private RunButton variable
+	private RunButton runButton = null;
+    // A private MeshInstance3D of StartButton to switch it's
+    // color on button press 
+	private MeshInstance3D sMeshInstance3D = new MeshInstance3D();
+    // A private StandardMaterial3D of StartButton to switch it's
+    // color on button press
+	private StandardMaterial3D sStandardMaterial3D = new StandardMaterial3D();
+    // A private PackedScene of our box that
+    // we will initialize
+    private PackedScene boxScene = new PackedScene();
+	// A private int variable of the length of Node3D list
+	private int lBoxesLenght = 0;
+	
 	public override void _Ready()
-	{
-		// Set a name for our box container
-		nodeBoxes.Name = "Boxes";
-		// Set our box containers position in the scene.
-		nodeBoxes.Position = new Vector3(0.0f, 0.0f, 0.0f);
-		// Wait for main Node3D to be free with CallDeferred()
-		// and then add our Node3D boxes to the scene
-		GetNode<Node3D>("../..").CallDeferred("add_child", nodeBoxes);
+	{	
+		// Get our RunButton for it's public variables
+		runButton = GetNode<RunButton>("../RunButton");
 		// Preload our box PackedScene
 		boxScene = GD.Load<PackedScene>("res://Assets/Box.tscn");
-		// Set the value into lBoxeslength of our listBoxes
-		lBoxeslength = listBoxes.Length;
-	}
+        // Get our StartButtons MeshInstace3D
+        sMeshInstance3D = GetNode<MeshInstance3D>("ButtonBody3D/MeshInstance3D");
+        // Give our scene Node3D container a name
+        node3DBoxes.Name = "Boxes";
+        // Give our scene Node3D container a position in the scene
+        node3DBoxes.Position = new Vector3(0.0f, 0.0f, 0.0f);
+        // Wait until Node3D main script is finished before
+        // adding our Node3D container to the scene with CallDeferred()
+        GetNode<Node3D>("../..").CallDeferred("add_child", node3DBoxes);
+        // Set the value of our Node3D list lenght to the lBoxesLenght variable
+        lBoxesLenght = lNode3DBoxes.Length;
+    }
 
 	public override void _Process(double delta)
 	{
-
+		// Switch case to see is our StartButton pressed on or off
 		switch(sButtonPressed)
 		{
+			// When the StartButton is off and it is pressed switch to on
 			case false when Input.IsActionJustPressed("Key_A"):
+				// Turn StartButton to green
+				sStandardMaterial3D.AlbedoColor = new Color(0.0f, 1.0f, 0.0f);
+				sMeshInstance3D.SetSurfaceOverrideMaterial(0, sStandardMaterial3D);
+				// Switch sButtonPressed from false to true
 				sButtonPressed = true;
-				GD.Print(sButtonPressed);                
-				break;
+                break;
+			// When the StartButton is on and it is pressed switch to off
 			case true when Input.IsActionJustPressed("Key_A"):
-				sButtonPressed = false;
-				GD.Print(sButtonPressed);
-				break;
-		}
-		if(sButtonPressed == true)
-		{
-			if (listBoxes[0] == null)
-			{
-				for(int i = 0; i <= lBoxeslength; i++)
+				// If RunButton is also turned on then turn it off
+				// otherwise just turn StartButton off
+				if(runButton.rButtonPressed == true)
 				{
-					listBoxes[i] = new Node3D();
-					box = boxScene.Instantiate<Node3D>();
-					box.Name = "Box_" + (i + 1);
-					box.Position = boxPos;
-					listBoxes[i] = box;
-				}
-			}
+					// Turn RunButton red
+					runButton.rStandardMaterial3D.AlbedoColor = new Color(1.0f, 0.0f, 0.0f);
+					runButton.rMeshInstace3D.SetSurfaceOverrideMaterial(0, runButton.rStandardMaterial3D);
+					// Switch rButtonPressed from true to false
+					runButton.rButtonPressed = false;
+					// Turn StartButton red
+					sStandardMaterial3D.AlbedoColor = new Color(1.0f, 0.0f, 0.0f);
+					sMeshInstance3D.SetSurfaceOverrideMaterial(0, sStandardMaterial3D);
+					// Switch sButtonPressed from true to false
+					sButtonPressed = false;
+					break;
+                }else
+				{
+                    // Turn StartButton red
+                    sStandardMaterial3D.AlbedoColor = new Color(1.0f, 0.0f, 0.0f);
+                    sMeshInstance3D.SetSurfaceOverrideMaterial(0, sStandardMaterial3D);
+                    // Switch sButtonPressed from true to false
+                    sButtonPressed = false;
+                    break;
+                }
 		}
 
-	}
+		// If StartButton is turned on. Instansiate the scene
+		// by loading the boxes into the Node3D list
+		if(sButtonPressed == true && lNode3DBoxes[0] == null)
+		{
+			for(int i = 0; i < lNode3DBoxes.Length; i++)
+			{
+				lNode3DBoxes[i] = new Node3D();
+				box = boxScene.Instantiate<Node3D>();
+				box.Name = "Box_" + (i + 1);
+				box.Position = pdSpawnPos;
+				lNode3DBoxes [i] = box;
+			}
+
+			// If the StartButton is turned off empty the box list
+		} else if(sButtonPressed == false && node3DBoxes.GetChildCount() == 0 && lNode3DBoxes[0] != null)
+		{
+			for(int i = 0; i < lNode3DBoxes.Length;i++)
+			{
+				lNode3DBoxes[i] = null;
+			}
+		}
+    }
 }
 
--- build/Assets/RunButton.cs
+++ build/Assets/RunButton.cs
@@ -1,96 +1,100 @@
 using Godot;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel.Design;
-using System.Linq;
 
 public partial class RunButton : Node3D
 {
-	// A private Node3D variable for our StartButton
-	private ButtonSpawn startButton = new ButtonSpawn();
-	// A private boolean value to see is button pressed
-	private bool RButtonPressed = false;
-	// A private Vector3 value to see is box spawn position free
-	private Vector3 SBoxPos = new Vector3(0.0f, 0.0f, 0.0f);
-	// A private compare value
-	private Vector3 ComBoxPos = new Vector3(0.0f, 0.0f, 0.0f);
-	// A private Vector3 list for box positions to go through
-	private Vector3[] LBoxPos = new Vector3[5];
-	// A private float time to make code wait a little before
-	// going forward
-	float time = 0.1f;
+	// A public MeshInstance3D of RunButton to switch it's
+	// color on button press
+	public MeshInstance3D rMeshInstace3D = new MeshInstance3D();
+	// A public StandardMaterial3D of RunButton to switch it's
+	// color on button press
+	public StandardMaterial3D rStandardMaterial3D = new StandardMaterial3D();
+	// A public boolean variable to see is button pressed
+	public bool rButtonPressed = false;
+	// A private StartButton variable
+	private ButtonSpawn startButton = null;
+	// A private Vector3 position variable to see in which
+	// position next box in the list is in the scene.
+	private Vector3 nBoxPos = new Vector3(0.0f, 0.0f, 0.0f);
+	// A private Vector3 position variable to
+	// see is box spawn position free
+	private Vector3 bSpawnPos = new Vector3(0.0f, 0.0f, 0.0f);
+    // A private list of boxes positions that we will go
+    // through to see that they are not in spawn point
+	private Vector3[] lBoxPos = new Vector3[5];
+	// A private float variable to time boxes movement
+	private float time = 0.0f;
 
 	public override void _Ready()
 	{
 		// Get our boxes from StartButton
 		startButton = GetNode<ButtonSpawn>("../StartButton");
-	}
+        // Get buttons meshInstace
+        rMeshInstace3D = GetNode<MeshInstance3D>("ButtonBody3D/MeshInstance3D");
+    }
 
 	public override void _Process(double delta)
 	{
-		// See if button B is pressed
-		switch(RButtonPressed)
+        // Switch case to see is our RunButton pressed on or off
+		switch(rButtonPressed)
 		{
-			case false when Input.IsActionJustPressed("Key_B"):
-				RButtonPressed = true;
-				GD.Print(RButtonPressed);
-				break;
-			case true when Input.IsActionJustPressed("Key_B"):
-				RButtonPressed = false;
-				GD.Print(RButtonPressed);
-				break;
-		}
-
-		if(RButtonPressed == true)
-		{
-			if (startButton.listBoxes[0] == null)
-			{
-				GD.Print("Start level first");
-				RButtonPressed = false;
-			}
-			else
-			{
-				for(int i = 0; i < startButton.listBoxes.Length; i++)
+            // When the RunButton is off and it is pressed switch to on
+            case false when Input.IsActionJustPressed("Key_B"):
+				// If StartButton is off then keep RunButton off and
+				// ask to turn StartButton on first. Otherwise continue normally
+				if(startButton.sButtonPressed == false)
 				{
-					if (SBoxPos.X <= (startButton.boxPos.X - 3.0f))
-					{
-						startButton.nodeBoxes.AddChild(startButton.listBoxes[i]);
-						LBoxPos[i] = startButton.listBoxes[i].Position;
-						SBoxPos = startButton.listBoxes[i].Position;
-						GD.Print("Loop number: " + i + "\n" + "Current box name: " + startButton.listBoxes[i].Name);
-						continue;
-					}
-					else if(SBoxPos.X <= startButton.boxPos.X)
-					{
-						for(int j = 0; j < LBoxPos.Length; j++)
-						{
-							if (LBoxPos[j] == ComBoxPos)
-							{
-								continue;
-							}
-							else
-							{
-								LBoxPos[j] -=  new Vector3(0.1f, 0.0f, 0.0f);
-								startButton.listBoxes[j].Position = LBoxPos[j];
-								SBoxPos = startButton.listBoxes[i].Position;
-								GD.Print("Loop number: " + i + "\n" + "List positions number: " + j + "\n" + "Position: " + LBoxPos[j]);
-								GD.Print(time);
-								if (time < 50000.0f)
-								{
-									time += time * (float)delta;
-								}
-								else if(time == 50000.0f)
-								{
-									GD.Print(time);
-									time = 0.1f;
-									continue;
-								}
-									
-							}
-						}
-					}
-
+					GD.Print("Please press the start button first");
+					break;
 				}
+				else
+				{
+                    // Turn RunButton to purple
+					rStandardMaterial3D.AlbedoColor = new Color(0.5f, 0.0f, 5.0f);
+					rMeshInstace3D.SetSurfaceOverrideMaterial(0, rStandardMaterial3D);
+                    // Switch rButtonPressed from false to true
+					rButtonPressed = true;
+                    break;
+                }
+            // When the RunButton is on and it is pressed switch to off
+            case true when Input.IsActionJustPressed("Key_B"):
+                // Turn RunButton red
+                rStandardMaterial3D.AlbedoColor = new Color(1.0f, 0.0f, 0.0f);
+                rMeshInstace3D.SetSurfaceOverrideMaterial(0, rStandardMaterial3D);
+                // Switch rButtonPressed from true to false
+                rButtonPressed = false;
+                break;
+		}
+		// If RunButton is turned on spawn the boxes into the scene
+		if(rButtonPressed == true)
+		{
+			time += (float)delta;
+			GD.Print(time);
+			for(int i = 0; i < startButton.lNode3DBoxes.Length; i++)
+			{
+				if(nBoxPos.X <= (startButton.pdSpawnPos.X - 3.0f))
+				{
+                    startButton.node3DBoxes.AddChild(startButton.lNode3DBoxes[i]);
+                    lBoxPos[i] = startButton.lNode3DBoxes[i].Position;
+                    nBoxPos = startButton.lNode3DBoxes[i].Position;
+                    continue;
+                } else if(nBoxPos.X <=startButton.pdSpawnPos.X)
+				{
+                    for (int j = 0; j < lBoxPos.Length; j++)
+                    {
+                        if (lBoxPos[j] == bSpawnPos)
+                        {
+                            continue;
+                        }
+						else
+						{
+                            lBoxPos[j] -= new Vector3(0.1f, 0.0f, 0.0f);
+                            startButton.lNode3DBoxes[j].Position = lBoxPos[j];
+                            nBoxPos = startButton.lNode3DBoxes[j].Position;
+                        }
+							
+                    }
+                }
 			}
 		}
 	}
